1fd275dbac15ecdae1aed0adaa15b7c82a3af798
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2023-11-13 23:18:56 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,10 +2,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import VideoViewSet
+from .views import video_list, upload_video
 
 router = DefaultRouter()
 router.register(r'videos', VideoViewSet, basename='video')
 
 urlpatterns = [
+    path('videos/', video_list, name='video-list'),
+    path('upload/', upload_video, name='upload-video'),
     path('', include(router.urls)),
 ]
