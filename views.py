161af468b3087cc3cdd7a136b1bbc86df87e0918
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2023-11-13 23:17:18 +0700

    Update views.py

--- views.py
+++ views.py
@@ -6,6 +6,10 @@ from rest_framework import viewsets
 from .models import Video
 from .serializers import VideoSerializer
 
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'videostream_app/video_list.html', {'videos': videos})
+
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
@@ -14,3 +18,24 @@ class VideoViewSet(viewsets.ModelViewSet):
         queryset = Video.objects.all()
         serializer = VideoSerializer(queryset, many=True)
         return render(request, 'videostream_app/video_list.html', {'videos': serializer.data})
+    
+# videostream_app/views.py
+
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'videostream_app/video_list.html', {'videos': videos})
+
+def upload_video(request):
+    if request.method == 'POST':
+        form = VideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('video-list')
+    else:
+        form = VideoForm()
+
+    return render(request, 'videostream_app/upload_video.html', {'form': form})
